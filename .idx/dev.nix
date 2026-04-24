f76bbbc7f3511fc801156b3058327b1cddeb96d4
chuquangthangftu <chuquangthangftu@gmail.com>
2026-04-24 15:47:45 +0000

    dev nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,7 +5,8 @@
   channel = "stable-24.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs_22
+    pkgs.nodejs_22,
+    pkgs.gh
   ];
   # Sets environment variables in the workspace
   env = {};
